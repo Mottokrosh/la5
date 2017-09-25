c9018953af0b676f26c7b9f4becf69e71d31a805
Frank Reding <frank.reding@gmail.com>
2017-09-25 20:30:09 +0200

    Basic links page

--- src/components/Links.vue
+++ src/components/Links.vue
@@ -1,5 +1,29 @@
 <template>
   <div>
     <h2>Links</h2>
+    <ul>
+      <li v-for="(link, index) in links" :key="index">
+        <a :href="link.url" target="_blank">{{ link.name }}</a> &mdash; {{ link.description }}
+      </li>
+    </ul>
   </div>
 </template>
+
+<script>
+  import axios from 'axios';
+
+  export default {
+    data() {
+      return {
+        links: [],
+      };
+    },
+
+    created() {
+      axios.get('/static/links.json')
+        .then((response) => {
+          this.links = response.data;
+        });
+    },
+  };
+</script>
