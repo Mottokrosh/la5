72180500af7ce8920d769f57f32742ded2fbaec9
Frank Reding <frank.reding@gmail.com>
2017-10-08 15:53:51 +0200

    Start models components

---
+++ src/components/Models.vue
@@ -0,0 +1,112 @@
+<template>
+  <div class="models">
+  </div>  
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        pictureWidth: 156,
+        pictureHeight: 156,
+        models: [
+          {
+            name: 'Eve',
+            picX: 0,
+            picY: 0,
+          },
+          {
+            name: 'Kittysocks',
+            picX: 156,
+            picY: 0,
+          },
+          {
+            name: 'Alais Peach',
+            picX: 312,
+            picY: 0,
+          },
+          {
+            name: 'Anastasia',
+            picX: 468,
+            picY: 0,
+          },
+          {
+            name: '',
+            picX: 0,
+            picY: 0,
+          },
+          {
+            name: '',
+            picX: 0,
+            picY: 0,
+          },
+          {
+            name: '',
+            picX: 0,
+            picY: 0,
+          },
+          {
+            name: '',
+            picX: 0,
+            picY: 0,
+          },
+          {
+            name: '',
+            picX: 0,
+            picY: 0,
+          },
+          {
+            name: '',
+            picX: 0,
+            picY: 0,
+          },
+          {
+            name: '',
+            picX: 0,
+            picY: 0,
+          },
+          {
+            name: '',
+            picX: 0,
+            picY: 0,
+          },
+          {
+            name: '',
+            picX: 0,
+            picY: 0,
+          },
+          {
+            name: '',
+            picX: 0,
+            picY: 0,
+          },
+          {
+            name: '',
+            picX: 0,
+            picY: 0,
+          },
+          {
+            name: '',
+            picX: 0,
+            picY: 0,
+          },
+          {
+            name: '',
+            picX: 0,
+            picY: 0,
+          },
+          {
+            name: '',
+            picX: 0,
+            picY: 0,
+          },
+          {
+            name: '',
+            picX: 0,
+            picY: 0,
+          },
+        ],
+      };
+    },
+  };
+</script>
