400b81eab4eda7422d7303919c7a5ade7a02fb3c
Frank Reding <frank.reding@gmail.com>
2017-10-08 18:23:20 +0200

    More models

--- src/components/Models.vue
+++ src/components/Models.vue
@@ -5,7 +5,7 @@
         <a href="#"><span>{{ model.name }}</span></a>
       </li>
     </ul>
-  </div>  
+  </div>
 </template>
 
 <script>
@@ -36,44 +36,44 @@
             picY: 0,
           },
           {
-            name: '',
-            picX: 0,
+            name: 'Freya',
+            picX: 624,
             picY: 0,
           },
           {
-            name: '',
-            picX: 0,
+            name: 'Jewell Marceau',
+            picX: 780,
             picY: 0,
           },
           {
-            name: '',
-            picX: 0,
+            name: 'Fayth on Fire',
+            picX: 936,
             picY: 0,
           },
           {
-            name: '',
-            picX: 0,
+            name: 'AltSiren',
+            picX: 1092,
             picY: 0,
           },
           {
-            name: '',
-            picX: 0,
+            name: 'Ammalia',
+            picX: 1248,
             picY: 0,
           },
           {
-            name: '',
-            picX: 0,
+            name: 'Caroline Pierce',
+            picX: 1404,
             picY: 0,
           },
           {
-            name: '',
+            name: 'Fay',
             picX: 0,
-            picY: 0,
+            picY: 312,
           },
           {
-            name: '',
-            picX: 0,
-            picY: 0,
+            name: 'Trisha',
+            picX: 156,
+            picY: 312,
           },
           {
             name: '',
