1d94f0fa4e46826e1f626899828cadd4f73f20ba
Frank Reding <frank.reding@gmail.com>
2017-10-09 20:39:37 +0200

    Navigation with popover

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -1,15 +1,15 @@
 <template>
   <nav class="main-navigation">
     <ul>
-      <li><router-link to="/home"><home-icon></home-icon> Home</router-link></li>
-      <li><router-link to="/about"><info-icon></info-icon> About</router-link></li>
-      <li><router-link to="/models"><users-icon></users-icon> Models</router-link></li>
+      <li><router-link to="/home"><home-icon></home-icon>Home</router-link></li>
+      <li><router-link to="/about"><info-icon></info-icon>About</router-link></li>
+      <li><router-link to="/models"><users-icon></users-icon>Models</router-link></li>
       <li>
-        <a href="#"><more-vertical-icon></more-vertical-icon> More</a>
-        <div class="popover">
-          <a href="http://blog.limitedaudience.com" target="_blank"><file-text-icon></file-text-icon> Blog</a>
-          <router-link to="/contact"><mail-icon></mail-icon> Contact</router-link>
-          <router-link to="/links"><link-icon></link-icon> Links</router-link>
+        <a href="#" @click.prevent="togglePopover"><more-vertical-icon></more-vertical-icon> More</a>
+        <div :class="popoverClasses" @click="popoverClick">
+          <a href="http://blog.limitedaudience.com" target="_blank">Blog<file-text-icon></file-text-icon></a>
+          <router-link to="/contact">Contact<mail-icon></mail-icon></router-link>
+          <router-link to="/links">Links<link-icon></link-icon></router-link>
         </div>
       </li>
     </ul>
@@ -28,6 +28,31 @@
       MailIcon,
       MoreVerticalIcon,
       UsersIcon,
+    },
+
+    data() {
+      return {
+        popoverActive: false,
+      };
+    },
+
+    methods: {
+      togglePopover() {
+        this.popoverActive = !this.popoverActive;
+      },
+
+      popoverClick() {
+        this.togglePopover();
+      },
+    },
+
+    computed: {
+      popoverClasses() {
+        return {
+          popover: true,
+          active: this.popoverActive,
+        };
+      },
     },
   };
 </script>
