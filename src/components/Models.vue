4d26fdf1572c308b1b51f65883547361619a1a0e
Frank Reding <frank.reding@gmail.com>
2017-10-12 19:47:53 +0200

    Fix model styling

--- src/components/Models.vue
+++ src/components/Models.vue
@@ -7,8 +7,11 @@
           :style="style(model)"
           @mouseover="hoverStyle(model, $event)"
           @mouseout="normalStyle(model, $event)"
+          @click.prevent="showModelDetails(model)"
         ></a>
-        <a href="#" class="name"><span>{{ model.name }}</span></a>
+        <a href="#" class="name" @click.prevent="showModelDetails(model)">
+          <span>{{ model.name }}</span>
+        </a>
       </li>
     </ul>
   </div>
@@ -24,24 +27,48 @@
         models: Store.state.models,
         pictureWidth: 156,
         pictureHeight: 156,
+        divider: 1,
       };
     },
 
     methods: {
       style(model) {
-        return `background-position: -${model.picX}px -${model.picY}px`;
+        return `background-position: -${model.picX / this.divider}px -${model.picY / this.divider}px`;
       },
 
       hoverStyle(model, $event) {
         const el = $event.target;
-        const y = model.picY + this.pictureHeight;
-        el.style = `background-position: -${model.picX}px -${y}px`;
+        const y = (model.picY / this.divider) + this.pictureHeight;
+        el.style = `background-position: -${model.picX / this.divider}px -${y}px`;
       },
 
       normalStyle(model, $event) {
         const el = $event.target;
-        el.style = `background-position: -${model.picX}px -${model.picY}px`;
+        el.style = `background-position: -${model.picX / this.divider}px -${model.picY / this.divider}px`;
       },
+
+      onWidthChange(mq) {
+        if (mq.matches) {
+          this.pictureWidth = 88;
+          this.pictureHeight = 88;
+          this.divider = 2;
+        } else {
+          this.pictureWidth = 156;
+          this.pictureHeight = 156;
+          this.divider = 1;
+        }
+      },
+
+      showModelDetails(model) {
+        // eslint-disable-next-line
+        console.log(model);
+      },
+    },
+
+    mounted() {
+      const mq = window.matchMedia('(max-width: 400px)');
+      mq.addListener(this.onWidthChange);
+      this.onWidthChange(mq);
     },
   };
 </script>
