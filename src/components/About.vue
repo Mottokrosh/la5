45b90d098ed39dba917ce19ab5f93bfc1c859130
Frank Reding <frank.reding@gmail.com>
2017-10-14 18:40:25 +0200

    Fix typo

--- src/components/About.vue
+++ src/components/About.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <h2>About Limited Audience</h2>
-    <p>Welcome to Limited Audience, Fashionable Bondage. This site collects material that was shot and a half decades. All of it was rigged, shot, and lovingly edited by myself, Frank. It features both professional and amateur models, but all of them were a joy to work with, and I'm forever proud of the amazing work we've done together.</p>
+    <p>Welcome to Limited Audience, Fashionable Bondage. This site collects material that was shot over one and a half decades. All of it was rigged, shot, and lovingly edited by myself, Frank. It features both professional and amateur models, but all of them were a joy to work with, and I'm forever proud of the amazing work we've done together.</p>
     <p><img src="/static/collage.jpg" alt="A collage of Limited Audience bondage pictures"></p>
     <p>Each trailer contains one or more links to places you can purchase the full video. You have my and my models' gratitude for each and every purchase!</p>
     <p>What started as a bondage tribute site in 2000, grew to one of the world's oldest bondage sites. Since the beginning it has featured original and unique content. It has gone through a multitude of redesigns over that time, and this is its latest incarnation&mdash;as easy to use on mobiles, as on tablets or desktop computers.</p>
