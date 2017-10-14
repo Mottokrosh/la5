463b2651054222aa6a374a3b9fc46563efe7d2fa
Frank Reding <frank.reding@gmail.com>
2017-10-14 18:35:29 +0200

    Basic About page

--- src/components/About.vue
+++ src/components/About.vue
@@ -1,5 +1,17 @@
 <template>
   <div>
     <h2>About Limited Audience</h2>
+    <p>Welcome to Limited Audience, Fashionable Bondage. This site collects material that was shot and a half decades. All of it was rigged, shot, and lovingly edited by myself, Frank. It features both professional and amateur models, but all of them were a joy to work with, and I'm forever proud of the amazing work we've done together.</p>
+    <p><img src="/static/collage.jpg" alt="A collage of Limited Audience bondage pictures"></p>
+    <p>Each trailer contains one or more links to places you can purchase the full video. You have my and my models' gratitude for each and every purchase!</p>
+    <p>What started as a bondage tribute site in 2000, grew to one of the world's oldest bondage sites. Since the beginning it has featured original and unique content. It has gone through a multitude of redesigns over that time, and this is its latest incarnation&mdash;as easy to use on mobiles, as on tablets or desktop computers.</p>
+    <p>Be sure to check back often&mdash;I have so much more material to put online!</p>
+    <h3>Interested?</h3>
+    <ul>
+      <li>The <router-link :to="{ name: 'Home' }">homepage</router-link> shows the totality of the content, spread over many pages</li>
+      <li>Click on a thumbnail to see video details, the trailer, and how to buy the full video</li>
+      <li>View the videos by <router-link :to="{ name: 'Models' }">model</router-link></li>
+      <li>Or, use the Search bar on the <router-link :to="{ name: 'Home' }">home page</router-link> to find what you're looking for!</li>
+    </ul>
   </div>
 </template>
